2f6e19b6b7288a68b599c7ed901639f244f8a263
Lamprey Sandor frey <fedorizmalkov@gmail.com>
2019-03-27 19:50:56 +0300

    1

--- 4 chapter/Vehicle.java
+++ 4 chapter/Vehicle.java
@@ -1,7 +1,14 @@
 class Vehicle{
     int passengers;
     int fuelcap;
-    int mpg;
+	int mpg;
+
+	Vehicle(int p, int f, int m)
+	{
+		passengers = p;
+		fuelcap = f;
+		mpg = m;
+	}
 
     int range(){
         return (fuelcap * mpg);
