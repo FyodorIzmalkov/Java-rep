4d73dbe16df936af42e16162ed9abfa65ce6d71b
Lamprey Sandor frey <fedorizmalkov@gmail.com>
2019-03-29 11:01:35 +0300

    1

---
+++ 5 chapter/NoZeroDiv2.java
@@ -0,0 +1,13 @@
+class NoZeroDiv2
+{
+	public static void main(String args[])
+	{
+		for (int i = -6; i < 5; i++)
+		{
+			if (i != 0 ? true : false)
+			{
+				System.out.println("100 / " + i + " is " + 100/ i);
+			}
+		}
+	}
+}
