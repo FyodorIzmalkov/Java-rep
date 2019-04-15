f91a9b838fd17e9d8c642632924e31b7513926bf
Teorell <fedorizmalkov@gmail.com>
2019-04-15 15:14:45 +0300

    1

---
+++ 9 chapter/ExcDemo1.java
@@ -0,0 +1,15 @@
+class ExcDemo1{
+    public static void main(String args[]){
+        int nums[] = new int[4];
+
+        try{
+            System.out.println("Before excp is generated.");
+            nums[7] = 10;
+            System.out.println("This wont be displayed;");
+        }
+        catch(ArrayIndexOutOfBoundsException exc){
+            System.out.println("Index out of bounds!");
+        }
+        System.out.println("After catch statement.");
+    }
+}
---
+++ 9 chapter/ExcDemo2.java
@@ -0,0 +1,23 @@
+class ExcTest{
+    static void genException(){
+        int nums[] = new int[4];
+
+        System.out.println("Before exc is generated.");
+        nums[7] = 10;
+        System.out.println("This wont be displayed!");
+        }
+}
+
+class ExcDemo2{
+    public static void main(String args[]){
+        try
+        {
+            ExcTest.genException();
+        }
+        catch (ArrayIndexOutOfBoundsException exc)
+        {
+            System.out.println("Index out-of-bounds!");
+        }
+        System.out.println("After catch statement.");
+    }
+}
