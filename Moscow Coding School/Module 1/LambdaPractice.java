f7098676f10eba7debb4c2fa3c9a779d96b2c5f2
Lamprey Sandor frey <fedorizmalkov@gmail.com>
2019-04-19 16:19:44 +0300

    1

--- Moscow Coding School/Module 1/LambdaPractice.java
+++ Moscow Coding School/Module 1/LambdaPractice.java
@@ -20,24 +20,16 @@ public class LambdaPractice{
 	public static void main(String args[]){
 
 		/*
-		** Задание 1.
-		**
-		** Создайте лямбду isIllegalString, принимающую множество символов и строку, проверяющую, содержит ли строка символы не из множества.
-		**
-		** Примеры:
-		** isIllegalString.test(Set.of('a', 'b'), "abc") == true
-		** isIllegalString.test(Set.of('a', 'b'), "ab")  == false
+		** First task.
 		*/
+
 		isIllegalString <Set<Character>, String> isillegalString = (ch, s)->s.chars().filter(c-> !ch.contains((char) c)).count() != 0;
 		System.out.println(isillegalString.test(Set.of('a','b'), "abc"));
 		System.out.println(isillegalString.test(Set.of('a','b'), "ab"));
 		System.out.println();
 
 		/*
-		** Задание 2.
-		**
-		** Напишите функцию printHello(), печатающую на стандартный поток вывода приветствие,
-		** и возвращающую void. Присвойте локальной переменной ссылку на эту функцию.
+		** Second task.
 		*/
 
 		LambdaPractice l = new LambdaPractice();
@@ -47,23 +39,10 @@ public class LambdaPractice{
 		System.out.println();
 		
 		/*
-		** Задание 3.
-		**
-		** Создайте лямбду printCode для применения к String, принимающую Character и печатающую код символа на экран (с пробелом).
-		**
-		** Пример: printCode.accept("abc") печатает 97 98 99
+		** Thrid task.
 		*/
-		
-		printCode <String> pc = str->{
-			for (int i = 0; i < str.length(); i++)
-			{
-				System.out.print((int)str.charAt(i));
-				if (i != str.length() - 1)
-					System.out.print(" ");
-			}
-			System.out.println();
-		};
-		pc.accept("abc");
 
+		printCode <String> pc = str->{for (int i = 0; i < str.length(); i++) System.out.print((int)str.charAt(i) + " ");};
+		pc.accept("abc");
 	}
 }
