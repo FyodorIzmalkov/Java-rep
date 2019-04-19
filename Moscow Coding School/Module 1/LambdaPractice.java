64f7d6b132f2699cb36720009b57cd1c18dbf1c7
Lamprey Sandor frey <fedorizmalkov@gmail.com>
2019-04-19 15:56:32 +0300

    1

--- Moscow Coding School/Module 1/LambdaPractice.java
+++ Moscow Coding School/Module 1/LambdaPractice.java
@@ -1,7 +1,4 @@
 import java.util.Set;
-import java.util.Arrays;
-
-
 public class LambdaPractice{
 
 	interface isIllegalString <T, U>{
@@ -22,12 +19,40 @@ public class LambdaPractice{
 	
 	public static void main(String args[]){
 
-		isIllegalString <Set<Character>, String> isillegalString = (ch, s)-> ch != null && s != null && !Arrays.equals(ch, s.toCharArray());
+		/*
+		** Задание 1.
+		**
+		** Создайте лямбду isIllegalString, принимающую множество символов и строку, проверяющую, содержит ли строка символы не из множества.
+		**
+		** Примеры:
+		** isIllegalString.test(Set.of('a', 'b'), "abc") == true
+		** isIllegalString.test(Set.of('a', 'b'), "ab")  == false
+		*/
+		isIllegalString <Set<Character>, String> isillegalString = (ch, s)->s.chars().filter(c-> !ch.contains((char) c)).count() != 0;
+		System.out.println(isillegalString.test(Set.of('a','b'), "abc"));
+		System.out.println(isillegalString.test(Set.of('a','b'), "ab"));
+		System.out.println();
+
+		/*
+		** Задание 2.
+		**
+		** Напишите функцию printHello(), печатающую на стандартный поток вывода приветствие,
+		** и возвращающую void. Присвойте локальной переменной ссылку на эту функцию.
+		*/
+
 		LambdaPractice l = new LambdaPractice();
 		MyInterface mi = ()->System.out.println("Hello world");
 		mi = l::printHello;
 		mi.run();
 		System.out.println();
+		
+		/*
+		** Задание 3.
+		**
+		** Создайте лямбду printCode для применения к String, принимающую Character и печатающую код символа на экран (с пробелом).
+		**
+		** Пример: printCode.accept("abc") печатает 97 98 99
+		*/
 		
 		printCode <String> pc = str->{
 			for (int i = 0; i < str.length(); i++)
