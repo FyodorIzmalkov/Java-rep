724bd2869ba69de4472a41f765956ec4cd532b16
Teorell <fedorizmalkov@gmail.com>
2019-05-12 21:50:12 +0300

    1

---
+++ 14 chapter/LambdaDemo3.java
@@ -0,0 +1,22 @@
+interface StringTest{
+    boolean test(String aStr, String bStr);
+}
+
+class LambdaDemo3{
+    public static void main(String args[]){
+        StringTest isIn = (a, b)->a.indexOf(b) != -1;
+
+        String str = "This is a test"; 
+        System.out.println("Testing string: " + str); 
+
+        if (isIn.test(str, "is a")) 
+            System.out.println("'is a' found."); 
+        else 
+            System.out.println("'is a' not found.");
+        
+        if (isIn.test(str, "xyz")) 
+            System.out.println("'xyz' Found"); 
+        else 
+            System.out.println("'xyz' not found"); 
+    }
+}
