77d79117bef7a3aa6fbfcb4439ddffc752753dbb
Teorell <fedorizmalkov@gmail.com>
2019-06-30 22:02:18 +0300

    1

---
+++ Collections framework/SberMapTwo.java
@@ -0,0 +1,92 @@
+public class SberMapTwo<K , V> {
+    public static final int DEFAULT_SIZE = 16;
+
+    private SberStore[] store;
+    private int size;
+
+
+    public SberMapTwo(){
+        this.store = new SberStore[DEFAULT_SIZE];
+    }
+
+    private class SberStore<K, V>{
+        private Node head;
+
+        private class Node{
+            K key;
+            V value;
+
+            Node next;
+
+            public Node(K key, V value){
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        public void add(K key, V value){
+            if (head == null) head = new Node(key, value);
+            else{
+                Node previous = null;
+                Node current = head;
+
+                while(current != null){
+                    if (current.key.equals(key)) {
+                        current.value = value;
+                        return;
+                    }
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = new Node(key, value);
+            }
+            size ++;
+        }
+
+        public V get(K key){
+            Node current = head;
+
+            while (current != null){
+                if (current.key.equals(key))
+                    break;
+                current = current.next;
+            }
+            return current == null ? null : current.value;
+        }
+    }
+
+    public void put(K key, V value){
+        int index = hash(key.hashCode());
+
+        if (store[index] == null)
+            store[index] = new SberStore();
+        store[index].add(key, value);
+    }
+
+    private int hash(int hashcode){
+        return (hashcode & 0x7fffffff) % store.length;
+    }
+
+    public V get(K key){
+        int index = hash(key.hashCode());
+
+        V value = null;
+        if (store[index] != null)
+            value = (V) store[index].get(key);
+        return value;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public static void main(String[] args) {
+        SberMapTwo<String, String> sberMapTwo = new SberMapTwo<>();
+
+        sberMapTwo.put("Hello world", "From russia");
+        sberMapTwo.put("Test1", "It is from test1");
+
+        System.out.println(sberMapTwo.get("Hello world"));
+        System.out.println(sberMapTwo.get("Test1"));
+    }
+}
